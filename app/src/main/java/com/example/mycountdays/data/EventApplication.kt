a0e972c9d8e19bb9524e3edec9a844ce5a911755
fk8663212 <66556154+fk8663212@users.noreply.github.com>
2025-04-02 01:36:31 +0800

    通知及詳細頁面

--- app/src/main/java/com/example/mycountdays/data/EventApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mycountdays.data
-
-import android.app.Application
-
-class EventApplication: Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
